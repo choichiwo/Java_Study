f4eeee4e3aa28b355ec9f62a2171a2238539798d
choichiwon <ccu12345@naver.com>
2021-07-22 17:20:05 +0900

    8교시 커밋

--- Study/src/HelloWorld.java
+++ Study/src/HelloWorld.java
@@ -27,6 +27,23 @@ public class HelloWorld {
 		
 		//Scanner s = new Scanner(System.in); 빈공간을 만들어서 입력하면 출력하는 기능
 		
+		// 문자열비교는 == 예외임 .equals 사용해야됨
+		
+//		switch(조건/표현식) {
+//		case 값/표현식:
+//			~~~~;
+//			break;
+//		case 값2/표현식2:
+//			~~~~;
+//			break;
+//		case 값3/표현식3:
+//			~~~~;
+//			break;
+//		default:
+//			~~~~~;
+//			break;
+//		}
+		
 	}
 
 }
---
+++ Study/src/Test02.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String str=s.nextLine();
+		if(str.equals("a") || str.equals("A")) {
+			System.out.println("Apple");
+		} else if(str.equals("b") || str.equals("B")) {
+			System.out.println("Banana");
+		} else if(str.equals("w") || str.equals("W"))  {
+			System.out.println("Watermelon");
+		} else if(str.equals("s") || str.equals("S")) {
+			System.out.println("Strawberry");
+		} else {
+			System.out.println("모르는 이니셜");
+		}
+		System.out.println("프로그램 종료");
+		
+		
+	}
+
+}
