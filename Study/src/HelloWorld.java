71be17a14f45fe78983236b445f7d23d06376d26
choichiwon <ccu12345@naver.com>
2021-07-22 10:28:35 +0900

    2교시 커밋

--- Study/src/HelloWorld.java
+++ Study/src/HelloWorld.java
@@ -2,8 +2,18 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int x;
+		x=10;
+		x=010; //8진수10= 10진수8
+		x=0xfff; // 16진수fff
+		float f;
+		f=3.14f;
+		char sch='x';
+		String str ="x"; 
+		// 문자데이터는 작은따움표로 묶는다. 'x','1'
+		// 문자열데이터는 큰따움표로 묶는다. "x","1","Hello"
+		// /n => new line
+		// /t => tab공간
 	}
 
 }
