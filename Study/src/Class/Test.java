4c47a8f2f95218f1719df3f9c20a79af03cd48c4
choichiwon <ccu12345@naver.com>
2021-07-27 10:35:08 +0900

    2교시 커밋

--- Study/src/Class/Test.java
+++ Study/src/Class/Test.java
@@ -3,7 +3,17 @@ package Class;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int p = 0;
+		for(int i=2; i<=10000; i++) {
+			for(int j=2; j<=i; j++) {
+				if(i%j==0)
+				p++;
+			}
+			if(p==1) {
+				System.out.println(i);
+			}
+			p = 0;
+		}
 
 	}
 
