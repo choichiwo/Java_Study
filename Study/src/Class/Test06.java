6e9de97f73f59a44c237699033b7779c1009c05c
choichiwon <ccu12345@naver.com>
2021-07-27 16:40:11 +0900

    7교시 커밋

---
+++ Study/src/Class/Test06.java
@@ -0,0 +1,21 @@
+package Class;
+
+public class Test06 {
+
+	public static void main(String[] args) {
+		// ¹è¿­ Array
+		int n = 5;
+		String[] str = new String[n];
+		str[0]="Hello";
+		str[1]="World";
+		str[2]="John";
+		str[3]="Jane";
+		str[4]="Jacob";
+		String x=str[2];
+		System.out.println(x);
+		
+		String[] xtr=new String[5];
+
+	}
+
+}
